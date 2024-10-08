975ac57aa2cdecb39205a6a53ffc6b9fb80f455d
SimonPfeifer <sikon08@hotmail.co.uk>
2024-10-08 16:25:19 +0200

    Move CSS definitions into global settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,14 +10,7 @@ export default defineConfig({
     integrations: [mdx(), sitemap(), tailwind()],
     markdown: {
         shikiConfig: {
-          // Choose from Shiki's built-in themes (or add your own)
-          // https://shiki.style/themes
-          // Alternatively, provide multiple themes
-          // See note below for using dual light/dark themes
-          themes: {
-            light: 'poimandres',
-            dark: 'catppuccin-latte',
-          },
+          theme: 'dracula-soft',
         },
       },
     
